3170a0c5371939038744627034981298636d0a5d
capsjac <capsjac>
2015-01-30 22:20:33 +0900

    doc

--- Data/Pack/ByteString.hs
+++ Data/Pack/ByteString.hs
@@ -15,6 +15,7 @@ module Data.Pack.ByteString
   , bytesBefore
   , cstring
   , varchar
+  --, signature
   , remainingBytes
   , remainingBytesCopy
   ) where
@@ -43,14 +44,17 @@ bytesCopy :: Int -> Packer ByteString
 bytesCopy n = fmap B.copy . bytes n
 {-# INLINE bytesCopy #-}
 
--- | A strict 'ByteString' 'Packet'.
+-- | 'bytesWhile', applied to a predicate p, returns the longest prefix
+-- (possibly empty) of bytes that satisfy p.
 bytesWhile :: (Word8 -> Bool) -> Packer ByteString
 bytesWhile pred = simpleBS $ \bs -> do
   let res = B.takeWhile pred bs
   return (B.length res, Right res)
 {-# INLINE bytesWhile #-}
 
--- | A strict 'ByteString' 'Packet'.
+-- | 'bytesBefore' slices remaining ByteString at the first occurence of the
+-- specified byte. It is more efficient than 'bytesWhile' as it is implemented
+-- with memchr(3).
 bytesBefore :: Word8 -> Packer ByteString
 bytesBefore word = simpleBS $ \bs -> do
   let res = fst $ B.break (== word) bs
@@ -88,6 +92,20 @@ varchar upperLimit value = flip simpleBS (pp value) $ \bs -> do
       let b = B.take upperLimit v
       in B.concat [b, B.replicate (upperLimit - B.length b) 0]
 {-# INLINE varchar #-}
+
+---- | Constant block of packet. Similar to 'unused' but specified ByteString
+-- will be used to fill out rather than NUL bytes. Additionally, read content
+-- is compared to the value and mismatch is reported.
+--signature :: ByteString -> Packer (Maybe ByteString)
+--signature upperLimit value = flip simpleBS (pp value) $ \bs -> do
+--  let field = B.take upperLimit bs
+--  let str = fst $ B.break (== 0) field
+--  return (B.length field, Right str)
+--  where
+--    pp v =
+--      let b = B.take upperLimit v
+--      in B.concat [b, B.replicate (upperLimit - B.length b) 0]
+--{-# INLINE signature #-}
 
 simpleBS :: (ByteString -> IO (Int, Either String ByteString)) -> Packer ByteString
 simpleBS get bs = asymmPacket get put size
