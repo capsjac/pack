c9a5da714f29a89def2f5e78d7c82366b9ab8137
capsjac <capsjac>
2015-03-12 17:02:15 +0900

    fix alignment, add helper functions

--- Data/Pack/Space.hs
+++ Data/Pack/Space.hs
@@ -13,6 +13,8 @@ module Data.Pack.Space
   , pad
   , alignedTo
   , alignedWith
+  , alignPos
+  , alignLen
   ) where
 
 import Control.Monad (replicateM_)
@@ -22,27 +24,34 @@ import Data.Word (Word8)
 
 
 -- | Skip bytes, filling with NUL bytes.
-unused :: Int -> Packet String ()
+unused :: Num a => a -> Packet String ()
 unused = pad 0
 {-# INLINE unused #-}
 
 -- | Skip bytes, filling with specified byte.
-pad :: Word8 -> Int -> Packet String ()
-pad filler n = replicateM_ n (simple 1 id id filler) -- XXX make this faster
+pad :: Num a => Word8 -> a -> Packet String ()
+pad filler n = replicateM_ (fromIntegral n) (simple 1 id id filler) -- XXX make this faster
 {-# INLINE pad #-}
 
 -- | (Unpacker only) Adjust alignment, filling with NUL bytes.
-alignedTo :: Int -> Packet String ()
+alignedTo :: Num a => a -> Packet String ()
 alignedTo = alignedWith 0
 {-# INLINE alignedTo #-}
 
 -- | (Unpacker only) Adjust alignment, filling with specified byte.
-alignedWith :: Word8 -> Int -> Packet String ()
+alignedWith :: Num a => Word8 -> a -> Packet String ()
 alignedWith filler blockSize = do
   pos <- getPosition
-  pad filler (mod pos blockSize)
+  pad filler (alignLen blockSize pos)
 {-# INLINE alignedWith #-}
 
---aligned :: Int -> Int -> Int
---aligned alignment n = (n + alignment - 1) .&. complement (alignment - 1)
+-- | Round up byte-offset to be aligned by the value specified in the first argument.
+alignPos :: (Num a, Bits a) => a -> a -> a
+alignPos alignment n = (n + alignment - 1) .&. complement (alignment - 1)
+{-# INLINE alignPos #-}
+
+-- | A number of padding bytes required to match alignment specified in the first argument.
+alignLen :: (Num a, Bits a) => a -> a -> a
+alignLen alignment pos = pos - alignedSize alignment pos
+{-# INLINE alignLen #-}
 
